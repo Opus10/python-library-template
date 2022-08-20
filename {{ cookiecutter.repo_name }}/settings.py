e00ac7758d17778fe2a08f1b98ce7c2d8a5f3a7d
Wes Kendall <wesleykendall@gmail.com>
2022-08-20 12:29:36 -0500

    Render release notes in Markdown, don't package tests, and use Black string formatting

--- {{ cookiecutter.repo_name }}/settings.py
+++ {{ cookiecutter.repo_name }}/settings.py
@@ -1,13 +1,13 @@
 import dj_database_url
 
 
-SECRET_KEY = '{{ cookiecutter.repo_name }}'
+SECRET_KEY = "{{ cookiecutter.repo_name }}"
 # Install the tests as an app so that we can make test models
 INSTALLED_APPS = [
-    '{{ cookiecutter.module_name }}',
-    '{{ cookiecutter.module_name }}.tests',
+    "{{ cookiecutter.module_name }}",
+    "{{ cookiecutter.module_name }}.tests",
 ]
 # Database url comes from the DATABASE_URL env var
-DATABASES = {'default': dj_database_url.config()}
+DATABASES = {"default": dj_database_url.config()}
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
